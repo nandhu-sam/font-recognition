2fae723e23617178646c0ebcb193f1c59f674355
Nandhu <77437049+nandhu-sam@users.noreply.github.com>
2023-09-15 13:27:16 +0530

    dataset-generator made

--- dataset-generator.py
+++ dataset-generator.py
@@ -2,12 +2,17 @@
 import os
 import glob
 
+import string
+
 import PIL.ImageFont
+import PIL.ImageDraw
 
 # import cv2 as cv
 
 FONT_SIZE_PT = 14
 
 font_dir = os.path.join('font-resources')
-font_fpaths = [PIL.ImageFont.truetype(fname, FONT_SIZE_PT)
-               for fname in glob.glob(os.path.join(font_dir, '*.ttf'))]
+font_fpaths = (PIL.ImageFont.truetype(fname, FONT_SIZE_PT)
+               for fname in glob.glob(os.path.join(font_dir, '*.ttf')))
+
+
