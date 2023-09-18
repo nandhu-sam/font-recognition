24da62dfc64f1b1104c79a8802ff9fb91efebd48
Nandhu Sam <77437049+nandhu-sam@users.noreply.github.com>
2023-09-18 12:38:32 +0530

    label named added

--- letter-classifier.py
+++ letter-classifier.py
@@ -13,20 +13,26 @@ import keras.models as models
 import keras.layers as layers
 import keras.preprocessing as preprocessing
 
+all_glyphs = [c+'-U+'+hex(ord(c))[2:] for c in string.ascii_letters + string.digits]
+
+
 train_ds, validation_ds = preprocessing.image_dataset_from_directory(
     path.Path('dataset') / 'train',
     label_mode='categorical',
+    class_names=all_glyphs,
     validation_split=(0.1 / 0.7),
     subset='both',
     shuffle=True,
     seed=random.SystemRandom().randint(0, 2 ** 32 - 1),
     color_mode='grayscale',
-    image_size=(32, 32)
+    image_size=(32, 32),
+
 )
 
 test_ds = preprocessing.image_dataset_from_directory(
     path.Path('dataset') / 'test',
     label_mode='categorical',
+    class_names=all_glyphs,
     shuffle=True,
     seed=random.SystemRandom().randint(0, 2 ** 32 - 1),
     color_mode='grayscale',
@@ -61,29 +67,8 @@ model.compile(loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 
 model.fit(train_ds,
-          epochs=int(input("epochs: ")),
+          epochs=40,
           validation_data=validation_ds
           )
 
-
-def labelFromVec(v):
-    ls = list(sorted(string.ascii_letters + string.digits))
-    return ls[list(v).index(1.0)]
-
-
-def toMaxVec(v):
-    l = np.array(list(v))
-    return (l>=l.max()).astype(np.float64)
-
-
-# print(np.concatenate([[y, model.predict(x)] for x, y in test_ds]).shape)
-# plt.imshow(tfmath.confusion_matrix(np.concatenate([y for _, y in test_ds]),np.concatenate([model.predict(x) for x, _ in test_ds])))
-
-plt.imshow(tfmath.confusion_matrix(tfmath.argmax(np.concatenate([model.predict(x) for x, y in test_ds]).T),
-                        tfmath.argmax(np.concatenate([y for x, y in test_ds]).T)), origin='lower', cmap='Greys_r')
-plt.show()
-
-
-
-
-plt.show()
+print(model.evaluate(test_ds))
