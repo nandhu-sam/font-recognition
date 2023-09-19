2706b8ac3500d0500aecfe353ca618d0da4f39cf
Nandhu <77437049+nandhu-sam@users.noreply.github.com>
2023-09-19 11:33:50 +0530

    saving accuracy plot for letter classifier model

--- letter-classifier.py
+++ letter-classifier.py
@@ -4,18 +4,21 @@ import pathlib as path
 import random
 import string
 
+import numpy as np
 import matplotlib.pyplot as plt
+
+import tensorflow as tf
 
 import keras.models as models
 import keras.layers as layers
 import keras.preprocessing as preprocessing
 
-all_glyphs = [c + '-U+' + hex(ord(c))[2:] for c in string.ascii_letters + string.digits]
+all_glyphs_classes = [c + '-U+' + hex(ord(c))[2:] for c in string.ascii_letters + string.digits]
 
 train_ds, validation_ds = preprocessing.image_dataset_from_directory(
     path.Path('dataset') / 'train',
     label_mode='categorical',
-    class_names=all_glyphs,
+    class_names=all_glyphs_classes,
     validation_split=(0.1 / 0.7),
     subset='both',
     shuffle=True,
@@ -28,7 +31,7 @@ train_ds, validation_ds = preprocessing.image_dataset_from_directory(
 test_ds = preprocessing.image_dataset_from_directory(
     path.Path('dataset') / 'test',
     label_mode='categorical',
-    class_names=all_glyphs,
+    class_names=all_glyphs_classes,
     shuffle=True,
     seed=random.SystemRandom().randint(0, 2 ** 32 - 1),
     color_mode='grayscale',
@@ -62,9 +65,20 @@ model = models.Sequential(
 model.compile(loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 
+epochs = 150
+
 history = model.fit(train_ds,
                     epochs=400,
                     validation_data=validation_ds)
 
+
+measures = np.c_[(history.history[k] for k in history.history.keys())]
+epoch_count = range(len(measures[0]))
+
+
+plt.plot(epoch_count, measures)
+
 model_save_path = path.Path('letter-model')
 model.save(model_save_path)
+
+
