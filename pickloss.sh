9235f97819eaaa02e888409ec1136e32e120b091
Nandhu Sam <77437049+nandhu-sam@users.noreply.github.com>
2023-09-30 15:00:32 +0530

    pickloss added

---
+++ pickloss.sh
@@ -0,0 +1,9 @@
+
+# opens only plots in Eye of GNOME
+
+for arg in "$@"
+do
+    fname=$(python3 -c "print(hex(ord('$arg'))[2:])")
+    eog "./evaluation-plots/loss/result-loss-$arg-U+$fname.svg" &
+done
+
