49323b6b4e5b5fe828cf7ff6f1f6ed9d719071a0
Nandhu Sam <77437049+nandhu-sam@users.noreply.github.com>
2023-10-05 15:31:09 +0530

    filters adjusted

--- letter-classifier.py
+++ letter-classifier.py
@@ -61,10 +61,10 @@ def main(img_shape=(64, 64)):
             layers.Conv2D(32, (3, 3), activation='relu'),  # Added for (64, 64) size
             layers.MaxPooling2D((2, 2)),
 
-            layers.Conv2D(32, (3, 3), activation='relu'),
+            layers.Conv2D(64, (3, 3), activation='relu'),
             layers.MaxPooling2D((2, 2)),
 
-            layers.Conv2D(32, (3, 3), activation='relu'),
+            layers.Conv2D(128, (3, 3), activation='relu'),
             layers.MaxPooling2D((2, 2)),
 
             layers.Flatten(),
